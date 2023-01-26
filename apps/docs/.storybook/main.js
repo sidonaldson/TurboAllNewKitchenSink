0dd1b8c5639106fcecbe95a779010baf067389b2
Si Donaldson <si.donalson@showhere.co>
2023-01-26 17:51:32 +0000

    chore: fix imports chore: fix storybook/jest error

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = {
   stories: ['../stories/**/*.stories.*', '../../../packages/**/*.stories.*'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite',
--- apps/docs/.storybook/preview.js
+++ apps/docs/.storybook/preview.js
@@ -1,1 +1,11 @@
-import "ui/styles.css";
+import 'ui/styles.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
