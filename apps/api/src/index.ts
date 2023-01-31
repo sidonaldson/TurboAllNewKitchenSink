687991af39ad76bda31b805ee20ab0a68e15a801
Si Donaldson <si.donalson@showhere.co>
2023-01-31 02:07:43 +0000

    chore: update ports

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import { createServer } from "./server";
 import { log } from "logger";
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const server = createServer();
 
 server.listen(port, () => {
