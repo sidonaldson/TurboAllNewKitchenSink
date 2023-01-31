b5d8a7acf7e9a0d20c1e2fe546602bb4deb41a9e
Si Donaldson <si.donalson@showhere.co>
2023-01-31 02:13:49 +0000

    chore: fix linting issues

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,5 @@
-import { createServer } from "./server";
 import { log } from "logger";
+import { createServer } from "./server";
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const port = process.env.PORT || 5000;
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -11,12 +11,8 @@ export const createServer = () => {
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    })
-    .get("/healthz", (req, res) => {
-      return res.json({ ok: true });
-    });
+    .get("/message/:name", (req, res) => res.json({ message: `hello ${req.params.name}` }))
+    .get("/healthz", (req, res) => res.json({ ok: true }));
 
   return app;
 };
