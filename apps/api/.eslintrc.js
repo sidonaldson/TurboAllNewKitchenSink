b51113eaf197123b6227ea1e9d9cdfa04443fb41
Si Donaldson <si.donalson@showhere.co>
2023-01-31 01:06:41 +0000

    chore: add parserOptions to all eslintrc

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
-  extends: ["custom-server"],
+  extends: ['custom-server'],
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
 };
--- apps/storefront/.eslintrc.js
+++ apps/storefront/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
-  extends: ["custom"],
+  extends: ['custom'],
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
 };
