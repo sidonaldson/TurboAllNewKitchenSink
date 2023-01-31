09d33ef1f64070f21e982cca38eedc83c5b20e14
Si Donaldson <si.donalson@showhere.co>
2023-01-31 01:24:07 +0000

    chore: add TS linting and airbnb code rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,12 +1,24 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    'next',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'turbo',
+    'prettier',
+  ],
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
   },
 };
