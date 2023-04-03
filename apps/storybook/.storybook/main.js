076b36f43b52497e2f4accfb9493ffbf385f3eb3
Ben-Swindells <bensterbas@gmail.com>
2023-04-03 15:06:51 +0100

    fix: upgrade storybook to v7

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,16 +1,17 @@
 const path = require("path");
-
 module.exports = {
   stories: ["../stories/**/*.stories.*", "../../../packages/**/*.stories.*"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
   ],
-  framework: "@storybook/react",
+  framework: "@storybook/react-vite",
   core: {
     builder: "@storybook/builder-vite",
   },
+
   async viteFinal(config, { configType }) {
     // customize the Vite config here
     return {
@@ -24,5 +25,8 @@ module.exports = {
         ],
       },
     };
+  },
+  docs: {
+    autodocs: true,
   },
 };
