3f8ef1148aac650320348666de5d6054ea9563b1
Tom van de Velde <tom@tomvdv.com>
2023-05-10 09:26:35 +0100

    Add support for Tailwind to `apps/presenter` (#2)
    Co-authored-by: Si Donaldson <si.donalson@showhere.co>

--- apps/presenter/src/App.tsx
+++ apps/presenter/src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
-import "./App.css";
 import { CounterButton, NewTabLink } from "ui";
+import "./App.css";
+import "ui/styles.css";
 
 function App() {
   return (
     <div className="container">
+      <p className="text-red-500">Hello Tailwind — this text should be red</p>
       <h1 className="title">
         Welcome to <br />
         <span>presenter</span>
